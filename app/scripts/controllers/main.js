33bee78752d271437b1e7eeb4129737695d9c1dd
Sean Helvey <sean.helvey@gmail.com>
2015-03-25 11:08:50 -0600

    basic crud with tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,30 @@
  */
 angular.module('gradesApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.studentName = '';
+    $scope.grade = '';
+    $scope.records = [];
+
+    $scope.addRecord = function () {
+        var newRecord = {
+            name: $scope.studentName,
+            grade: $scope.grade,
+        };
+        $scope.records.push(newRecord);
+    };
+
+    $scope.deleteRecord = function (index) {
+        $scope.records.splice(index, 1);
+    };
+
+    $scope.calculateAverage = function(MyData){
+        var sum = 0;
+        var avg = 0;
+        for(var i = 0; i < MyData.length; i++){
+            sum += parseInt(MyData[i].grade, 10);
+            avg = sum/MyData.length;
+        }
+        return avg;
+    };
+
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,19 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach an empty list of records to the scope', function () {
+    expect(scope.records.length).toBe(0);
   });
+
+  it('should add two elemets to records', function () {
+    scope.addRecord({james:10});
+    scope.addRecord({james:10});
+    expect(scope.records.length).toBe(2);
+  });
+
+  it('should remove one element from records', function () {
+    scope.deleteRecord(1);
+    expect(scope.records.length).toBe(1);
+  });
+
 });
