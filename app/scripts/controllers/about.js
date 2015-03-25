d280b1df7ded0c427316ff7b5c68aa1c8b1f2084
Sean Helvey <sean.helvey@gmail.com>
2015-03-24 19:59:28 -0600

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gradesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gradesApp
+ */
+angular.module('gradesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
