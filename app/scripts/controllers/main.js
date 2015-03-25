70dcefec74a7eff91a6dbd64a81bb94ff4bd20f4
Sean Helvey <sean.helvey@gmail.com>
2015-03-25 15:17:41 -0600

    improving tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,18 +11,34 @@ angular.module('gradesApp')
   .controller('MainCtrl', function ($scope) {
     $scope.studentName = '';
     $scope.grade = '';
+    $scope.average = 0;
+    $scope.minimum = 0;
+    $scope.maximum = 0;
     $scope.records = [];
 
-    $scope.addRecord = function () {
+    $scope.$watch('records.length', function(){
+        $scope.calculateStatistics($scope.records);
+    });
+
+    $scope.addRecord = function (studentName, grade) {
         var newRecord = {
-            name: $scope.studentName,
-            grade: $scope.grade,
+            name: studentName,
+            grade: grade
         };
         $scope.records.push(newRecord);
+        $scope.grade = '';
+        $scope.studentName = '';
+        angular.element('#studentName').focus();
     };
 
     $scope.deleteRecord = function (index) {
         $scope.records.splice(index, 1);
+    };
+
+    $scope.calculateStatistics = function() {
+        $scope.average = $scope.calculateAverage($scope.records);
+        $scope.minimum = $scope.calculateMinimum($scope.records);
+        $scope.maximum = $scope.calculateMaximum($scope.records);
     };
 
     $scope.calculateAverage = function(MyData){
@@ -33,6 +49,29 @@ angular.module('gradesApp')
             avg = sum/MyData.length;
         }
         return avg;
+    };
+
+    $scope.calculateMinimum = function(MyData){
+        var min = 99999999;
+        for(var i = 0; i < MyData.length; i++){
+            if (MyData[i].grade < min) {
+                min = MyData[i].grade;
+            }
+        }
+        if (min === 99999999) {
+            min = 0;
+        }
+        return min;
+    };
+
+    $scope.calculateMaximum = function(MyData){
+        var max = 0;
+        for(var i = 0; i < MyData.length; i++){
+            if (MyData[i].grade > max) {
+                max = MyData[i].grade;
+            }
+        }
+        return max;
     };
 
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,19 +16,78 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach an empty list of records to the scope', function () {
+  it('should calculate statistics correctly with empty list', function () {
     expect(scope.records.length).toBe(0);
+    expect(scope.average).toBe(0);
+    expect(scope.minimum).toBe(0);
+    expect(scope.maximum).toBe(0);
   });
 
-  it('should add two elemets to records', function () {
-    scope.addRecord({james:10});
-    scope.addRecord({james:10});
+  it('should calculate statistics correctly after adding and removing 1 record', function () {
+    scope.addRecord('james',10);
+    scope.deleteRecord(0);
+    scope.$digest();
+    expect(scope.records.length).toBe(0);
+    expect(scope.average).toBe(0);
+    expect(scope.minimum).toBe(0);
+    expect(scope.maximum).toBe(0);
+  });
+
+  it('should calculate statistics correctly after adding 3 records', function () {
+    scope.addRecord('james',10);
+    scope.addRecord('karen',20);
+    scope.addRecord('jimmy',30);
+    scope.$digest();
+    expect(scope.records.length).toBe(3);
+    expect(scope.average).toBe(20);
+    expect(scope.minimum).toBe(10);
+    expect(scope.maximum).toBe(30);
+  });
+
+  it('should calculate statistics correctly after adding 3 records and removing 1', function () {
+    scope.addRecord('james',10);
+    scope.addRecord('karen',20);
+    scope.addRecord('jimmy',30);
+    scope.deleteRecord(0);
+    scope.$digest();
     expect(scope.records.length).toBe(2);
+    expect(scope.average).toBe(25);
+    expect(scope.minimum).toBe(20);
+    expect(scope.maximum).toBe(30);
   });
 
-  it('should remove one element from records', function () {
-    scope.deleteRecord(1);
+  it('should calculate statistics correctly after adding a score of 0', function () {
+    scope.addRecord('jimmy',0);
+    scope.$digest();
     expect(scope.records.length).toBe(1);
+    expect(scope.average).toBe(0);
+    expect(scope.minimum).toBe(0);
+    expect(scope.maximum).toBe(0);
+  });
+
+  it('should calculate statistics correctly after adding 3 records including a score of 0', function () {
+    scope.addRecord('james',10);
+    scope.addRecord('karen',20);
+    scope.addRecord('jimmy',0);
+    scope.$digest();
+    expect(scope.records.length).toBe(3);
+    expect(scope.average).toBe(10);
+    expect(scope.minimum).toBe(0);
+    expect(scope.maximum).toBe(20);
+  });
+
+  it('should calculate statistics correctly after adding 3 records and removing all of them', function () {
+    scope.addRecord('james',10);
+    scope.addRecord('karen',20);
+    scope.addRecord('jimmy',0);
+    scope.deleteRecord(0);
+    scope.deleteRecord(0);
+    scope.deleteRecord(0);
+    scope.$digest();
+    expect(scope.records.length).toBe(0);
+    expect(scope.average).toBe(0);
+    expect(scope.minimum).toBe(0);
+    expect(scope.maximum).toBe(0);
   });
 
 });
